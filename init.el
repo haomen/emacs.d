bb0bd44a7cbef21c0534b109d25556b5b8ac3ddf
Hao Men [xxx][==========/ <hao.men@gmail.com>
2018-03-06 22:49:47 -0500

    udpate init.el

--- init.el
+++ init.el
@@ -1,6 +1,8 @@
 (require 'package)
-(add-to-list 'package-archives
-	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list
+ 'package-archives
+ '("melpa" . "http://melpa.org/packages/") t)
+;; '("milkbox" . "http://melpa.milkbox.net/packages/") t)
 (package-initialize)
 
 (setq gc-cons-threshold 100000000)
@@ -197,3 +199,28 @@
   "major-mode for markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+(add-to-list 'load-path "~/.emacs.d/custom")
+(autoload 'web-mode "web-mode" "web-mode to edit html and js" t)
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (web-mode zygospore yasnippet ws-butler volatile-highlights undo-tree sr-speedbar smartparens iedit icicles helm-swoop helm-projectile helm-gtags ggtags duplicate-thing dtrt-indent company comment-dwim-2 clean-aindent-mode anzu))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
